7856789328314e51c501729ecf974798f9490c98
Raldunateav <Ricardo.Aldunate@fundacion-jala.org>
2022-12-09 20:15:16 -0400

    dino1.1

---
+++ dino_runner/components/obstacles/bird.py
@@ -0,0 +1,11 @@
+from dino_runner.components.obstacles.obstacle import Obstacle
+
+class Bird(Obstacle):
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+        
+    def draw(self, screen):
+        pass
---
+++ dino_runner/components/obstacles/cactus.py
@@ -0,0 +1,15 @@
+import random
+from dino_runner.components.obstacles.obstacle import Obstacle
+
+class Cactus(Obstacle):
+    
+    def __init__(self, images):
+        type = random.randint(0,2)
+        super().__init__(images, type)
+        self.rect.y = 325
+
+    def update(self):
+        pass
+        
+    def draw(self, screen):
+        pass
